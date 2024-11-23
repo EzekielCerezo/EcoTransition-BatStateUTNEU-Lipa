526d57f96bf5e6f0769c209014caf3c956b26d30
Ezekiel Eisen Cerezo <21-34994@g.batstate-u.edu.ph>
2024-11-23 19:11:15 +0800

    Added Media Query in Login Page

--- instance/config.py
+++ instance/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
     SQLALCHEMY_DATABASE_URI = os.getenv(
